de84c1d1c9f875585435842c050a3ed1b7c3481d
Anubhavpandey27 <anubhavpandey.bt22cseds@pec.edu.in>
2023-12-23 14:57:26 +0530

    23 dec 2023

---
+++ 363 div 1/A.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <math.h>
+vector<pair<int,int>> helper(vector<int> vec,vector<pair<int,int>>&dp,int &ans,int i){
+ if(vec[i]=)
+}
+
+int main(){
+int t;
+cin>>t;
+vector<int> vec;
+vector<pair<int,int>> dp(t+1,make_pair(-1,-1));
+for(int j=0;j<t;j++){
+    int ele;
+    cin>>ele;
+    vec.emplace_back(ele);
+    
+    
+
+}
+
+
+
+}
